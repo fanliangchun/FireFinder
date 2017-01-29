372af9555fd46e56a66567259664d6e684480afb
fanliangchun <jeremydo2010@gmail.com>
2017-01-29 10:18:32 +0800

    implement admin/apps CURD

---
+++ app/controllers/admin/apps_controller.rb
@@ -0,0 +1,51 @@
+class Admin::AppsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :find_app, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@apps = App.all
+	end
+
+	def show
+	end
+
+	def new
+		@app = App.new
+	end
+
+	def create
+		@app = App.new(app_params)
+
+		if @app.save
+			redirect_to admin_apps_path
+		else
+			render :new
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @app.update(app_params)
+			redirect_to admin_apps_path, notice: "App Updated"
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@app.destroy
+		redirect_to admin_apps_path, alert: "App Deleted"
+	end
+
+	private
+
+	def app_params
+		params.require(:app).permit(:title, :description)
+	end
+
+	def find_app
+		@app = App.find(params[:id])
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :apps
   root 'apps#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+  	resources :apps
+  end
+
 end
