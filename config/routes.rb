95f0307e5b13ce1764fa0ae1e0f87cd2abb1ace5
fanliangchun <jeremydo2010@gmail.com>
2017-02-02 16:01:43 +0800

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	devise_for :users
+	resources :about
+	resources :welcome
+	resources :apps do
+		resources :comments
+		collection do
+			get :search
+		end
+	end
+	root 'welcome#index'
+
+	namespace :admin do
+		resources :apps do
+			member do
+				post :publish
+				post :hide
+			end
+		end
 end
