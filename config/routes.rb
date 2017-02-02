395d2f42d9b60dbe33eb05b31a695e8e10c9b7e5
fanliangchun <jeremydo2010@gmail.com>
2017-02-02 13:14:55 +0800

    heroku error

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :welcome
-  resources :about
-  resources :apps do
-    resources :comments
-    collection do
-      get :search
-    end
-
-    member do
-      post 'upvote'
-    end
-  end
-  root 'welcome#index'
-
-  namespace :admin do
-  	resources :apps do
-  		member do
-  			post :publish
-  			post :hide
-  		end
-  	end
-  end
-
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
