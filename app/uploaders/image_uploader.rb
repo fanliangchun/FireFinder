e430a6fb7236c39477ffaeaf3b4e58b48daf11d0
fanliangchun <jeremydo2010@gmail.com>
2017-02-02 23:13:02 +0800

    fix uploader and show

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -14,6 +14,14 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
+
+CarrierWave.configure do |config|
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
+end
+
   process resize_to_fit: [400, 400]
 
   version :thumb do
